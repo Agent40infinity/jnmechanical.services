342efab0c245007aec887a1b5d8c76649cdcd0e7
Aiden Nathan <agent9@y7mail.com>
2025-11-16 01:56:25 +1100

    Test please

---
+++ js/Preloader.js
@@ -0,0 +1,36 @@
+﻿function Rendered(duration) {
+    OnImagesLoaded(duration);
+}
+
+function Loaded(duration) {
+    $('.pre-loader').fadeOut(duration);
+
+    $('.pre-loader').removeClass("faded-black")
+}
+
+function Unloaded(duration) {
+    $('.pre-loader').fadeIn(duration);
+
+    $('.pre-loader').addClass("faded-black")
+}
+
+function OnImagesLoaded(duration) {
+    var images = document.getElementsByTagName("img");
+    var loaded = images.length;
+    for (var i = 0; i < images.length; i++) {
+        if (images[i].complete) {
+            loaded--;
+        }
+        else {
+            images[i].addEventListener("load", function () {
+                loaded--;
+                if (loaded <= 0) {
+                    Loaded(duration);
+                }
+            });
+        }
+    }
+    if (loaded <= 0) {
+        Loaded(duration);
+    }
+}
